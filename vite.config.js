933069fd5f70ec1d320bdc239a134282e774d7e5
xuweidong <13016742879@163.com>
2022-08-04 00:09:54 +0800

    项目初始化完成 EsLint Less配置成功

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
+  // 配置less
+  css: {
+    preprocessorOptions: {
+      less: {
+        additionalData: '@import "@/assets/index.less";'
+      }
+    }
+  }
 })
