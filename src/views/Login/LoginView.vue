06dd7c2d2e10aa71dec23e2d4cf1277876b77760
Yefan <Yefan97114@qq.com>
2022-08-04 20:32:40 +0800

    完成login

--- src/views/Login/LoginView.vue
+++ src/views/Login/LoginView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="perBox">
+    <div>{{ percent <= 100 ? '网页加载中，请稍等...' : '已加载完毕' }}</div>
     <a-tooltip title="正在加载中">
       <a-progress :percent="percent" :success="{ percent: percent }" />
     </a-tooltip>
@@ -23,12 +24,42 @@
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, watch, h, defineComponent } from 'vue'
+import { notification, Button, message } from 'ant-design-vue'
+import { useRouter } from 'vue-router'
+const router = useRouter()
 const percent = ref(0)
 onMounted(() => {
   setInterval(() => {
     percent.value++
   }, 25)
+})
+watch(percent, () => {
+  if (percent.value === 100) {
+    // 提示并跳转路由
+    const key = `open${Date.now()}`
+    notification.open({
+      message: '请问是否需要我来帮你跳转到主页？',
+      description: '点击确定立即跳转',
+      btn: () =>
+        h(
+          Button,
+          {
+            type: 'primary',
+            size: 'small',
+            onClick: () => {
+              console.log(1111)
+              router.push('/home')
+            }
+          },
+          { default: () => '跳转主页' }
+        ),
+      key,
+      onClose: () => {
+        message.error('暂不跳转')
+      }
+    })
+  }
 })
 </script>
 
@@ -44,3 +75,51 @@ onMounted(() => {
   }
 }
 </style>
+
+<!-- 
+
+<template>
+  <a-button type="primary" @click="openNotification">Open the notification box</a-button>
+</template>
+<script lang="ts">
+import { notification, Button } from 'ant-design-vue';
+import { h, defineComponent } from 'vue';
+
+const close = () => {
+  console.log(
+    'Notification was closed. Either the close button was clicked or duration time elapsed.',
+  );
+};
+export default defineComponent({
+  setup() {
+    const openNotification = () => {
+      const key = `open${Date.now()}`;
+      notification.open({
+        message: 'Notification Title',
+        description:
+          'A function will be be called after the notification is closed (automatically after the "duration" time of manually).',
+        btn: () =>
+          h(
+            Button,
+            {
+              type: 'primary',
+              size: 'small',
+              onClick: () => notification.close(key),
+            },
+            { default: () => 'Confirm' },
+          ),
+        key,
+        onClose: close,
+      });
+    };
+
+    return {
+      openNotification,
+    };
+  },
+});
+</script>
+
+
+
+ -->
