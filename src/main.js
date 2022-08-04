e699cf429b508981281f54a51bb44bf11b568410
Yefan <Yefan97114@qq.com>
2022-08-04 12:35:48 +0800

    新增ele组件库

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import './style.css'
 import './assets/index.less'
 import App from './App.vue'
 
+// element组件UI库
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
 // 导入路由
 import router from '@/router'
 // 导入pinia
@@ -12,4 +16,6 @@ const pinia = createPinia()
 const app = createApp(App)
 app.use(router)
 app.use(pinia)
+
+app.use(ElementPlus)
 app.mount('#app')
